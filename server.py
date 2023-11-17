d7f45ea3d7bf4b0cda626e2a3055211912d32021
polichao <kurasarylor@gmail.com>
2023-11-17 14:27:08 +0300

    Add convertion to json before send info to DB. And changed the url of DB to internal from render

--- server.py
+++ server.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify
 import redis
 
 app = Flask(__name__)
-redis_url = 'rediss://red-cl0jd2c8s0fs73csl1e0:7VdBBf0LN3eez6C9ZrBd4hr0w6xbso4n@oregon-redis.render.com:6379'
+redis_url = 'redis://red-cl0jd2c8s0fs73csl1e0:6379'
 redis_client = redis.Redis.from_url(redis_url, decode_responses=True)
 @app.route('/get_data', methods=['GET'])
 def get_data():
@@ -19,16 +19,23 @@ def get_data():
 
 @app.route('/set_data', methods=['POST'])
 def set_data():
-    # Example POST request to store data in Redis
-    data = request.get_json()
-    key = data.get('key')
-    value = data.get('value')
+    try:
+        data = request.get_json()
+        key = data.get('key')
+        value = data.get('value')
 
-    if key and value:
-        redis_client.set(key, value)
-        return jsonify({'success': True})
-    else:
-        return jsonify({'error': 'Missing key or value parameters'}), 400
+        if key and value:
+            # Serialize the value (nested dictionary) to a JSON string
+            json_value = json.dumps(value)
+
+            # Store the JSON string in Redis
+            redis_client.set(key, json_value)
+
+            return jsonify({'success': True})
+        else:
+            return jsonify({'error': 'Missing key or value parameters'}), 400
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
     app.run(debug=True)
